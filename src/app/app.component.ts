d1b12f35c48f31deeadf453a3c6ffcd8a58ca102
Notsume <adahorapirola@gmail.com>
2024-03-13 13:07:19 -0300

    card tour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { TravelsComponent } from './components/travels/travels.component';
+import { ToursComponent } from './components/tours/tours.component';
+import { HotelsComponent } from './components/hotels/hotels.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, TravelsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, TravelsComponent, ToursComponent, HotelsComponent, BlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/tours/tours.component.ts
+++ src/app/components/tours/tours.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './tours.component.scss'
 })
 export class ToursComponent {
+  cards = [
+    { id: 1, image: 'tour-sp.jpg', name: "São Paulo: Guided tour of the city's highlights", time: '5 hours', extra: 'Search service included', local: 'São Paulo - Brazil', type: 'Guided Tour', price: '€63.19 per person' },
+    { id: 2, image: 'tour-tokyo.jpg', name: 'From Tokyo: Full-Day Mount Fuji Sightseeing Tour', time: '10 hours', extra: '', local: 'Tokyo - Japan', type: '1 day tour', price: '€75.05 per person' },
+    { id: 3, image: 'tour-warner.jpg', name: 'London: Warner Bros. Studios Tour with transfers', time: '7.5 hours', extra: '', local: 'London - England', type: '1 day tour', price: '€122.94 per person' }
+  ];
 
+  
 }
