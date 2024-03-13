eea967ed767fbcb9bc162132113bb8dc6ead0054
Notsume <adahorapirola@gmail.com>
2024-03-13 15:56:37 -0300

    alteração no heading do home e mais um tour adicionado

--- src/app/components/tours/tours.component.ts
+++ src/app/components/tours/tours.component.ts
@@ -11,7 +11,8 @@ export class ToursComponent {
   cards = [
     { id: 1, image: 'tour-sp.jpg', name: "São Paulo: Guided tour of the city's highlights", time: '5 hours', extra: 'Search service included', local: 'São Paulo - Brazil', type: 'Guided Tour', price: '€63.19 per person' },
     { id: 2, image: 'tour-tokyo.jpg', name: 'From Tokyo: Full-Day Mount Fuji Sightseeing Tour', time: '10 hours', extra: '', local: 'Tokyo - Japan', type: '1 day tour', price: '€75.05 per person' },
-    { id: 3, image: 'tour-warner.jpg', name: 'London: Warner Bros. Studios Tour with transfers', time: '7.5 hours', extra: '', local: 'London - England', type: '1 day tour', price: '€122.94 per person' }
+    { id: 3, image: 'tour-warner.jpg', name: 'London: Warner Bros. Studios Tour with transfers', time: '7.5 hours', extra: '', local: 'London - England', type: '1 day tour', price: '€122.94 per person' },
+    { id: 4, image: 'tour-rio.jpg', name: 'Arraial do Cabo', time: '10 hours', extra: 'Air-conditioned transport • Boat ride • Lunch Included', local: 'Rio de Janeiro - Brazil', type: '1 day tour', price: '€49,58 per person' }
   ];
 
   
